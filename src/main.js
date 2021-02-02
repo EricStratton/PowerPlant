d997bf231f0a2ca0d5d8d13a5112b45502333e0c
Eric Stratton <strattonericj@gmail.com>
2021-02-02 13:50:21 -0800

    install dependencies, add simple html

--- src/main.js
+++ src/main.js
@@ -1,27 +1,5 @@
-// blueFood = (state) => ("soil") => {
-//   return (5) => {
-//     return (state) => ({
-//       ...state,
-//       ["soil"] : (state["soil"] || 0) + 5
-//     })
-//   }
-// }
 
-// blueFood(cannabisPlant) leads to 5 units of whatever being added for its growth
-
-// yellowFood = changeState("soil")(3);
-
-// yellowFood(cannabisPlant) lead to 3 units of whatever being added
-
-
-
-// yellowStrain1 = changeState("soil")(3);
-// yellowStrain31 = changeState("soil")(2);
-
-// const newCannabisPlant = yellowStrain1(cannabisPlant);
-
-
-
+// Business Logic //
 
 // Function Factory // for altering state
 
@@ -30,19 +8,51 @@ const changeState = (prop) => {
     return (state) => ({
       ...state,
       [prop]: (state[prop] || 0) + value
-    })
-  }
-}
+    });
+  };
+};
 
 // Store State // stores and updates state of an object ??
 
 const storeState = () => {
   let currentState = {}; // between function steps
-  return (stateChangeFunction) => {
+  return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState); // call changeState function on current state and save as the new state
     currentState = {...newState}; // save currentState as a copy of newState // so that next time this is called it's a mirror of the current, updated state?
     return newState;
-  }
-}
+  };
+};
 
 const stateControl = storeState();
+
+// User Interface Logic //
+
+const feed = changeState("soil")(1);
+const superFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+const sun = changeState("light")(5);
+
+
+$(document).ready(function() {
+
+  $('#feed').click(function() {
+    const newState = stateControl(superFood);
+    4('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+
+  $('#show-state').click(function() {
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`)
+  });
+
+
+});
+
+feed;
+superFood;
+hydrate;
+superWater;
+sun;
